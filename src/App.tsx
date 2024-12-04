b28fc9dc40e9ff2f46e0648b279f9834f030832f
Rony Arya Wijaya <rony.wijaya@ccn.com.sg>
2024-12-04 22:29:25 +0700

    add description flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Steps } from 'antd';
 
 const submissionStatus = 'SUBMITTED'
 const appFormStatus = 'EXPIERED'
 const illusStatus = 'ERROR'
 const App: React.FC = () => {
-
   return (
     <Steps
       className='steps-illustration'
       items={[
+        // Flow codenya jadi gini, di itemnya punya classname masing2 yang bakal ngatur style connectornya,
+        // nah jadi di spaj sama ilustration itu ngecek dulu status di step depannya null apa ga ? kalo ga null kita aktifin connectornya dengan cara kasih classNamenya 
         {
           title: 'Ilustration',
           status: 'error',
-          className: appFormStatus !== null ? 'illusItem' : '',
+          className: appFormStatus !== null ? 'illusItem' : '', // step ilus ngecek status spaj null atau ga
           description: illusStatus
         },
         {
           title: 'SPAJ',
-          status: 'error',
-          className: submissionStatus !== null ? 'spajItem ' : '',
+          status: 'wait',
+          className: submissionStatus !== null ? 'spajItem ' : '', // step spaj ngecek status submssion null atau ga
           description: appFormStatus
         },
         {
